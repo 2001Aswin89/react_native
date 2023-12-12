661b8a6f9941b9e15c35d9bd320f74987d3bd9fd
Aswin Sivadas <2001Aswin89@gmail.com>
2023-12-12 23:49:19 +0530

    first commit

---
+++ assets/css/style.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from 'react-native';
+
+export const commonStyles = StyleSheet.create({
+  keyboard__container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: '400px',
+    width: '100%',
+  },
+  button__num: {
+    width: "30px",
+    height: "30px",
+  }
+});
---
+++ components/keypad.js
@@ -0,0 +1,21 @@
+import { StyleSheet, Text, View ,Button} from 'react-native';
+import { commonStyles } from '../assets/css/style'
+export default function Keyboard() {
+  return (
+    <>
+        <div style={commonStyles.keyboard__container} class="keyboard__container">
+        <Button class="button__num" value="1" title='1' style={commonStyles.button__num}></Button>
+        <Button class="button__num" value="2" title='2' style={commonStyles.button__num}></Button>
+        <Button class="button__num" value="3" title='3' style={commonStyles.button__num}></Button>
+        <Button class="button__num" value="4" title='4' style={commonStyles.button__num}></Button>
+        <Button class="button__num" value="5" title='5' style={commonStyles.button__num}></Button>
+        <Button class="button__num" value="6" title='6' style={commonStyles.button__num}></Button>
+        <Button class="button__num" value="7" title='7' style={commonStyles.button__num}></Button>
+        <Button class="button__num" value="8" title='8' style={commonStyles.button__num}></Button>
+        <Button class="button__num" value="9" title='9' style={commonStyles.button__num}></Button>
+        <Button class="button__num" value="0" title='0' style={commonStyles.button__num}></Button>
+
+        </div>
+    </>
+  )
+}
